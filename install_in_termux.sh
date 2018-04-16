131e65b8d597618ca53dd0d7b40a160367f7a9b0
Shafley <shafleyxia@gmail.com>
2018-04-16 10:35:55 +0800

    add install script

---
+++ install_in_termux.sh
@@ -0,0 +1,19 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+apt update
+apt install -y git zsh
+clear
+
+if [ -d "$HOME/.termux" ]; then
+ mv $HOME/.termux $HOME/.termux.bak
+fi
+
+git clone https://github.com/Shafley/oh-my-zsh.git $HOME/.oh-my-zsh --depth 1
+cp $HOME/.oh-my-zsh/templates/zshrc.zsh-template $HOME/.zshrc
+chsh -s zsh
+
+termux-setup-storage
+
+echo Done!
+
+exit
