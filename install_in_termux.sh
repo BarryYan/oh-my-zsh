89159c5d70a845e8f1ad37109af310e35d730499
Barry <barry.yansh@gmail.com>
2019-05-07 18:46:18 +0800

    Update install_in_termux.sh

--- install_in_termux.sh
+++ install_in_termux.sh
@@ -1,14 +1,13 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-apt update
-apt install -y git zsh
+pkg install git zsh
 
 if [ -d "$HOME/.termux" ]; then
  mv $HOME/.termux $HOME/.termux.bak
 fi
 
-git clone https://github.com/Shafley/oh-my-zsh.git $HOME/.oh-my-zsh --depth 1
-cp $HOME/.oh-my-zsh/templates/zshrc.zsh-template $HOME/.zshrc
+git clone https://github.com/BarryYan/oh-my-zsh.git $HOME/.oh-my-zsh --depth=1
+cp $HOME/.oh-my-zsh/templates/zshrc.zsh-m-template $HOME/.zshrc
 chsh -s zsh
 
 termux-setup-storage
